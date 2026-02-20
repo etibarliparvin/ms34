8e40bf9ee30b00a263b8abbf0743b4e6b11d967b
Parvin Etibarli <etibarliparvin@gmail.com>
2026-02-20 15:50:13 +0400

    nermin2

--- src/main/java/com/appsdeveloperblog/ws/Main.java
+++ src/main/java/com/appsdeveloperblog/ws/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Parvin3");
         System.out.println("git");
         System.out.println("nermin1");
+        System.out.println("nermin2");
     }
 }
